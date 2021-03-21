3e555c7b93a5b61f04e3c25805300bc781d4baf5
nomyfan <Nomyfan@live.com>
2021-03-21 13:53:25 +0800

    feat: step IV

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+/**
+ * Create element description including type, props and children,
+ * which is used to create fibers.
+ */
 export function createElement(type, props, ...children) {
   children = children.map((child) =>
     typeof child === "object" ? child : createTextElement(child)
@@ -27,7 +31,86 @@ function createTextElement(text) {
   };
 }
 
+/**
+ * Create DOM from fiber
+ */
+function createDom(fiber) {
+  const dom =
+    fiber.type === "TEXT_ELEMENT"
+      ? document.createTextNode("")
+      : document.createElement(fiber.type);
+
+  const isProperty = (key) => key !== "children";
+  Object.keys(fiber.props)
+    .filter(isProperty)
+    .forEach((name) => (dom[name] = fiber.props[name]));
+
+  return dom;
+}
+
+// unit of work means fiber
 let nextUnitOfWork = null;
+
+function render(element, container) {
+  // root fiber
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+/**
+ * Attach DOM node to fiber and construct the fiber tree
+ */
+function performUnitOfWork(fiber) {
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  const elements = fiber.props.children;
+  let prevSibling = null;
+
+  for (let index = 0; index < elements.length; index++) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    // Kinda like InnoDB leaf nodes
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  for (let nextFiber = fiber; nextFiber; ) {
+    // If there's any slibling, it's the next unit of work
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    // Otherwise, lookup fiber's parent's sliblings
+    nextFiber = nextFiber.parent;
+  }
+  // We will reach there when we reach to root fiber(root fiber has no parent and siblings)
+}
 
 function workLoop(deadline) {
   let shouldYield = false;
@@ -39,27 +122,7 @@ function workLoop(deadline) {
   requestIdleCallback(workLoop);
 }
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
-}
-
 requestIdleCallback(workLoop);
-
-function render(element, container) {
-  const dom =
-    element.type === "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => (dom[name] = element.props[name]));
-
-  element.props.children.forEach((child) => render(child.type, dom));
-
-  container.appendChild(dom);
-}
 
 export const YoReact = {
   createElement,
